ba047eb358e9257ed2344d16c5f3bc11e6de11d4
Kazuki Shimizu <kazubu@jtime.net>
2022-02-17 10:00:00 -0800

    implemented priovisioning server discovery

--- run.py
+++ run.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
+from logging import getLogger
+
 import os
+import re
 import sys
+
 import dns.resolver
 
+FALLBACK_DNS_SERVERS = ['2404:1a8:7f01:a::3', '2404:1a8:7f01:b::3']
 
+logger = getLogger(__name__)
+
+def discover_provisioning_server(nameservers = None):
+    resolver = dns.resolver.Resolver()
+    if nameservers != None:
+        resolver.nameservers = nameservers
+
+    try:
+        response_txt = str(resolver.resolve('4over6.info', 'TXT')[0])[1:-1]
+
+    except dns.resolver.NoAnswer as e:
+        if nameservers == None:
+            logger.warning("No response with system DNS server. Retry with NGN DNS server.")
+            return discover_provisioning_server(FALLBACK_DNS_SERVERS)
+        else:
+            logger.error("No response.")
+            return None
+
+    response_list = re.split('[\s=]', response_txt)
+    result = {response_list[i]: response_list[i + 1] for i in range(0, len(response_list), 2)}
+
+    return result
 
 if __name__ == '__main__':
+    print(discover_provisioning_server())
     exit()
 
